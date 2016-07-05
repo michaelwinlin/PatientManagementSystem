4d37e95f3569171ec24bb2cc83264485d6c3fa44
michaelwinlin <michaelwinlin@gmail.com>
2016-07-05 23:34:05 +0800

    Update Bed.py

--- Bed.py
+++ Bed.py
@@ -1,6 +1,3 @@
-# Student Number : S10165254A , S10127952K
-# Student Name   : Lin XiongQing and Lin Yu Hsien
-# Module Group   : FI 04
 
 from abc import ABCMeta,abstractmethod
 class Bed(metaclass=ABCMeta):
